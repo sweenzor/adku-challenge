d0f80f9b76d912a68df9eaced5d1753d3b1e887b
mattswe <mattswe@gmail.com>
2010-12-16 08:16:50 -0800

    Defined gridspace, first function to move within it

--- walk.py
+++ walk.py
@@ -1,9 +1,21 @@
 import numpy
 
 space = numpy.zeros((5,5))
-space[0,0] = 1
+#space[0,0] = 1
+locationstack = [[0,0]]
+
+for coordinate in locationstack:
+	space[coordinate[0], coordinate[1]] = 1
 
 
+def moves(stack):
+	current_coordinate = stack[-1]
+	new[0] = current_coordinate[0] + 1
+	new[1] = current_coordinate[1] + 1
+	stack.append(new)
+
+moves(locationstack)
+	
 #def move_up:
 	
 
