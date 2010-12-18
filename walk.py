25490b57689714deec8d5854ded60b430e25416e
mattswe <mattswe@gmail.com>
2010-12-18 14:48:11 -0800

    bound checking on next steps

--- walk.py
+++ walk.py
@@ -1,13 +1,13 @@
 import numpy
 
-def print_stack(stack):	
+def print_stack(stack,num):	
 	space = numpy.zeros((5,5))
 	for int in stack:
-		space[int[0],int[1]] = 1
+		space[int[0],int[1]] = num
 	print space
 	
-stack = [[1,1],[2,2],[3,3]]
-print_stack(stack)
+stack = [[1,1],[2,2],[4,3]]
+print_stack(stack,1)
 	
 	
 def next_steps(stack):
@@ -16,8 +16,15 @@ def next_steps(stack):
 	potential.append([stack[-1][0]-1,stack[-1][1]])
 	potential.append([stack[-1][0],stack[-1][1]+1])
 	potential.append([stack[-1][0],stack[-1][1]-1])
-	print_stack(potential)
 	
-
+	for next_step in potential:
+		if next_step[0] < 0: potential.remove(next_step)
+		if next_step[0] > 4: potential.remove(next_step)
+		if next_step[1] < 0: potential.remove(next_step)
+		if next_step[1] > 4: potential.remove(next_step)
+		for step in stack:
+			
 	
+	print_stack(potential,2)
+		
 next_steps(stack)
