2df7385be9d179e51d1dc7b06230acecf0b1ef80
mattswe <mattswe@gmail.com>
2010-12-18 14:24:53 -0800

    blanked

--- walk.py
+++ walk.py
@@ -1,22 +1,10 @@
 import numpy
 
-space = numpy.zeros((5,5))
-#space[0,0] = 1
-locationstack = [[0,0]]
-
-for coordinate in locationstack:
-	space[coordinate[0], coordinate[1]] = 1
-
-
-def moves(stack):
-	current_coordinate = stack[-1]
-	new[0] = current_coordinate[0] + 1
-	new[1] = current_coordinate[1] + 1
-	stack.append(new)
-
-moves(locationstack)
+def print_stack(stack):	
+	space = numpy.zeros((5,5))
+	for int in stack:
+		space[int[0],int[1]] = 1
+	print space
 	
-#def move_up:
-	
-
-print space
+stack = [[1,1],[2,2],[3,3]]
+print_stack(stack)
