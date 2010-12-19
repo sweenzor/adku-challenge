f8c891360b17dde4bbd3dafe1ff3d1638f5bade4
mattswe <mattswe@gmail.com>
2010-12-18 16:49:01 -0800

    slice the list whole list first[:] slow, but should be ok

--- walk.py
+++ walk.py
@@ -14,40 +14,26 @@ def next_steps(stack):
 	potential.append([stack[-1][0]-1,stack[-1][1]])
 	potential.append([stack[-1][0],stack[-1][1]+1])
 	potential.append([stack[-1][0],stack[-1][1]-1])
-	
-	print potential
-	
-	#li = [ x for x in li if condition(x)]
-	
-	for next_step in potential:
+
+	for next_step in potential[:]:
 		for step in stack:
 			if next_step == step: potential.remove(next_step)
-		print next_step[0], next_step[1]
 		if next_step[0] < 0: potential.remove(next_step)
 		if next_step[0] > 4: potential.remove(next_step)
 		if next_step[1] < 0: potential.remove(next_step)
 		if next_step[1] > 4: potential.remove(next_step)
-		print potential
-
 
 	return potential
 
 
+paths = [[[0,0],[0,1]],[[0,0],[1,0]]]
 
-a = [[0, 0], [0, 1], [1, 1], [1, 2], [0, 2]]
-print_stack(a,1)
-next_steps(a)
-
-	
-
-# paths = [[[0,0],[0,1]],[[0,0],[1,0]]]
-
-# while True:
-	# steps = paths.pop(0)
-	# new_steps = next_steps(steps)
-	# for step in new_steps:
-		# paths.append(steps+[step])
-	# for path in paths:
-		# print path
-	# print '\n'
+while True:
+	steps = paths.pop(0)
+	new_steps = next_steps(steps)
+	for step in new_steps:
+		paths.append(steps+[step])
+	for path in paths:
+		print path
+	print '\n'
 
